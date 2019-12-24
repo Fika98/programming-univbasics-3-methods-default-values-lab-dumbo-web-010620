2147645c9f32c2f4cf92fcf1d5e236a693b6ba51
Firuz Afzalov <afzalov98@gmail.com>
2019-12-24 06:10:45 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,8 +6,9 @@ puts "What a nutritious meal!"
 
 puts "A plate of #{protein} with #{veg1} and #{veg2}."
 
-return
- sum = "A plate of #{protein} with #{veg1} and #{veg2}."
+
+ "A plate of #{protein} with #{veg1} and #{veg2}."
+ 
 
 
 
